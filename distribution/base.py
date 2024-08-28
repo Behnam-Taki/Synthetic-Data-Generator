1e267a3038548e2389774c0da8d60158a543edc7
Behnam Taki <behnamtaki90@gmail.com>
2024-08-29 00:00:51 +0330

    Remove param holder from distribution structure

--- distribution/base.py
+++ distribution/base.py
@@ -4,31 +4,15 @@ from typing import Any, Type
 
 
 class Distribution(ABC):
-    class ParameterHolder(ABC):
-        @classmethod
-        @abstractmethod
-        def get_random_param_holder(cls) -> Distribution.ParameterHolder:
-            raise NotImplementedError()
-
-        @abstractmethod
-        def derivate_param_holder(self, distance: float) -> Distribution.ParameterHolder:
-            raise NotImplementedError()
-
-    def __init__(self, params: ParameterHolder):
-        self._params = params
-
     @classmethod
     @abstractmethod
-    def _get_param_holder_class(cls) -> Type[Distribution.ParameterHolder]:
-        raise NotImplementedError()
-
-    @classmethod
     def get_random_distribution(cls) -> Distribution:
-        return cls(cls._get_param_holder_class().get_random_param_holder())
+        raise NotImplementedError()
 
     @abstractmethod
     def generate_sample(self) -> Any:
         raise NotImplementedError()
 
+    @abstractmethod
     def generate_child(self, distance: float) -> Distribution:
-        return self.__class__(self._params.derivate_param_holder(distance))
+        raise NotImplementedError()
