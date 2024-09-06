c05c2de63bb48a0ad4c11a94ee7c4b8060a6ca77
Behnam Taki <behnamtaki90@gmail.com>
2024-09-06 17:12:00 +0330

    Implement word distribution

--- distribution/text.py
+++ distribution/text.py
@@ -1,15 +1,21 @@
-from typing import Any
+from typing import Any, Self
 
 from .base import Distribution
+from .general import MarkovDistribution
 
 
 class WordDistribution(Distribution):
+    def __init__(self, character_markov_dist: MarkovDistribution):
+        self.character_markov_dist = character_markov_dist
+
     @classmethod
-    def get_random_distribution(cls) -> Distribution:
-        pass
+    def get_random_distribution(cls, *args, **kwargs) -> Self:
+        states = [chr(i+ord('a')) for i in range(26)] + ['$']
+        return WordDistribution(character_markov_dist=MarkovDistribution.get_random_distribution(states=states))
 
     def generate_sample(self) -> Any:
-        pass
+        char_sequence = self.character_markov_dist.generate_sample()
+        return ''.join(char_sequence)
 
-    def generate_child(self, distance: float) -> Distribution:
+    def generate_child(self, distance: float) -> Self:
         pass
