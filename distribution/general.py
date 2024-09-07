4120d62c76c428367460a3b5db6aa7d295e03fdd
Behnam Taki <behnamtaki90@gmail.com>
2024-09-07 17:47:02 +0330

    Add Generic types and other typing improvements

--- distribution/general.py
+++ distribution/general.py
@@ -1,5 +1,5 @@
 import math
-from typing import List, Any, Self
+from typing import List, Self, TypeVar, Generic
 
 import numpy as np
 
@@ -18,7 +18,7 @@ class GaussianDistribution(Distribution):
             variance=np.random.normal(loc=2, scale=1)
         )
 
-    def generate_sample(self, *args, **kwargs) -> Any:
+    def generate_sample(self, *args, **kwargs) -> float:
         return np.random.normal(loc=self.mean, scale=math.sqrt(self.variance))
 
     def generate_child(self, distance: float) -> Self:
@@ -33,24 +33,27 @@ class PoissonDistribution(Distribution):
     def get_random_distribution(cls) -> Self:
         return PoissonDistribution(lam=np.random.gamma(shape=10, scale=0.5))
 
-    def generate_sample(self, *args, **kwargs) -> Any:
+    def generate_sample(self, *args, **kwargs) -> int:
         return np.random.poisson(lam=self.lam)
 
     def generate_child(self, distance: float) -> Self:
         raise NotImplementedError()
 
 
-class CategoricalDistribution(Distribution):
-    def __init__(self, states: List[Any], probabilities: np.ndarray):
+CategoryType = TypeVar('CategoryType')
+
+
+class CategoricalDistribution(Distribution, Generic[CategoryType]):
+    def __init__(self, states: List[CategoryType], probabilities: np.ndarray):
         self.states = states
         self.probabilities = probabilities
         self._cdf = self.probabilities.cumsum()
 
     @classmethod
-    def get_random_distribution(cls, states: List[Any]) -> Self:
+    def get_random_distribution(cls, states: List[CategoryType]) -> Self:
         return CategoricalDistribution(states, np.random.dirichlet(np.ones(len(states)), size=1))
 
-    def generate_sample(self, *args, **kwargs) -> Any:
+    def generate_sample(self, *args, **kwargs) -> CategoryType:
         r = np.random.uniform()
         index = np.searchsorted(self._cdf, r)
         return self.states[index]
@@ -59,10 +62,13 @@ class CategoricalDistribution(Distribution):
         raise NotImplementedError()
 
 
-class MarkovDistribution(Distribution):
-    def __init__(self, states: List[Any],
-                 initial_distribution: CategoricalDistribution,
-                 transition_distributions: List[CategoricalDistribution],
+StateType = TypeVar('StateType')
+
+
+class MarkovDistribution(Distribution, Generic[StateType]):
+    def __init__(self, states: List[StateType],
+                 initial_distribution: CategoricalDistribution[StateType],
+                 transition_distributions: List[CategoricalDistribution[StateType]],
                  final_state_index=None
                  ):
         """
@@ -78,7 +84,7 @@ class MarkovDistribution(Distribution):
         self.final_state_index = final_state_index if final_state_index is not None else len(states) - 1
 
     @classmethod
-    def get_random_distribution(cls, states: List[Any], final_state_index=None) -> Self:
+    def get_random_distribution(cls, states: List[StateType], final_state_index=None) -> Self:
         indices_list = list(range(len(states)))
         initialable_indices_list = indices_list.copy()
         initialable_indices_list.pop(final_state_index if final_state_index is not None else -1)
@@ -88,8 +94,8 @@ class MarkovDistribution(Distribution):
             [CategoricalDistribution.get_random_distribution(indices_list) for _ in states]
         )
 
-    def _generate_next_state(self, current_state_index: int = None, allow_final=True):
-        next_state_distribution = (
+    def _generate_next_state(self, current_state_index: int = None, allow_final=True) -> int:
+        next_state_distribution: CategoricalDistribution[StateType] = (
             self.transition_distributions[current_state_index] if current_state_index else
             self.initial_distribution)
         while True:
@@ -97,7 +103,7 @@ class MarkovDistribution(Distribution):
             if allow_final or result != self.final_state_index:
                 return result
 
-    def generate_sample(self, lenght: int = None) -> List[Any]:
+    def generate_sample(self, lenght: int = None) -> List[StateType]:
         sample = []
         allow_final = lenght is None
         current_state_index = self._generate_next_state(allow_final=allow_final)
--- distribution/text.py
+++ distribution/text.py
@@ -1,11 +1,11 @@
-from typing import Any, Self, Dict, List
+from typing import Self, Dict, List, TypeVar, Generic, Set
 
 from .base import Distribution
 from .general import MarkovDistribution, PoissonDistribution, CategoricalDistribution
 
 
 class WordDistribution(Distribution):
-    def __init__(self, character_markov_dist: MarkovDistribution, length_dist: PoissonDistribution):
+    def __init__(self, character_markov_dist: MarkovDistribution[str], length_dist: PoissonDistribution):
         self.character_markov_dist = character_markov_dist
         self.length_dist = length_dist
 
@@ -15,7 +15,7 @@ class WordDistribution(Distribution):
         return WordDistribution(character_markov_dist=MarkovDistribution.get_random_distribution(states=states),
                                 length_dist=PoissonDistribution.get_random_distribution())
 
-    def generate_sample(self, *args, **kwargs) -> Any:
+    def generate_sample(self, *args, **kwargs) -> str:
         char_sequence = self.character_markov_dist.generate_sample(lenght=self.length_dist.generate_sample() + 1)
         return ''.join(char_sequence)
 
@@ -23,42 +23,45 @@ class WordDistribution(Distribution):
         pass
 
 
-class WordPosDistribution(Distribution):
+PosType = TypeVar('PosType')
+
+
+class WordPosDistribution(Distribution, Generic[PosType]):
     def __init__(self,
                  word_distribution: WordDistribution,
-                 pos_distribution: CategoricalDistribution,
+                 pos_distribution: CategoricalDistribution[PosType],
                  wordset_size: int):
         self.word_distribution = word_distribution
         self.pos_distribution = pos_distribution
         self.wordset_size = wordset_size
-        pos_wordsets: Dict[str, List] = {pos: [] for pos in pos_distribution.states}
-        empty_pos = set(pos_wordsets.keys())
+        pos_wordsets: Dict[PosType, List[str]] = {pos: [] for pos in pos_distribution.states}
+        empty_pos: Set[PosType] = set(pos_wordsets.keys())
         for i in range(wordset_size):
             if i + len(empty_pos) >= wordset_size:
                 break
-            word = self.word_distribution.generate_sample()
-            pos = self.pos_distribution.generate_sample()
+            word: str = self.word_distribution.generate_sample()
+            pos: PosType = self.pos_distribution.generate_sample()
             pos_wordsets[pos].append(word)
             if pos in empty_pos:
                 empty_pos.remove(pos)
         for pos in empty_pos:
-            word = self.word_distribution.generate_sample()
+            word: str = self.word_distribution.generate_sample()
             pos_wordsets[pos].append(word)
-        self.pos_word_dists: Dict[str, CategoricalDistribution] = {
+        self.pos_word_dists: Dict[PosType, CategoricalDistribution[str]] = {
             pos: CategoricalDistribution.get_random_distribution(states=word_list)
             for pos, word_list in pos_wordsets.items()
         }
 
     @classmethod
-    def get_random_distribution(cls, pos_list: List[str], wordset_size=None) -> Self:
-        wordset_size = wordset_size or PoissonDistribution(500).generate_sample()
+    def get_random_distribution(cls, pos_list: List[PosType], wordset_size=None) -> Self:
+        wordset_size: int = wordset_size or PoissonDistribution(500).generate_sample()
         return WordPosDistribution(
             word_distribution=WordDistribution.get_random_distribution(),
             pos_distribution=CategoricalDistribution.get_random_distribution(states=pos_list),
             wordset_size=wordset_size
         )
 
-    def generate_sample(self, pos: str) -> Any:
+    def generate_sample(self, pos: PosType) -> str:
         return self.pos_word_dists[pos].generate_sample()
 
     def generate_child(self, distance: float) -> Self:
