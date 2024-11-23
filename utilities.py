97e2ccf1e8503f24752110bc7b597b4b96c70e83
Behnam Taki <behnamtaki90@gmail.com>
2024-11-24 00:27:34 +0330

    Add bin_search_on_answer

--- utilities.py
+++ utilities.py
@@ -1,4 +1,24 @@
+from typing import Callable
+
+
 def to_sub_description(description: str, format_for_one_line: bool = False) -> str:
     return ('\n' + description).replace('\n', '\n   ') \
         if '\n' in description or format_for_one_line \
         else ' ' + description
+
+
+def bin_search_on_answer(
+        func: Callable[[float], float],
+        search_for: float,
+        start: float,
+        end: float,
+        precision=1e-3
+) -> float:
+    func_is_acsending = func(start) < func(end)
+    while end - start > precision:
+        mid = (end + start) / 2
+        if (func(mid) > search_for and func_is_acsending) or (func(mid) < search_for and not func_is_acsending):
+            end = mid
+        else:
+            start = mid
+    return start
